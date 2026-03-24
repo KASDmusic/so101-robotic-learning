a9a1b3f76e21d4a61fdfa4febe27e9b4d1664ea8
kasdmusic <lecoindre.kenzo@gmail.com>
2026-03-24 03:53:54 +0100

    correction erreur debug.py

--- scripts/bounce_rl/debug.py
+++ scripts/bounce_rl/debug.py
@@ -5,33 +5,27 @@ import mujoco.viewer
 import sys
 from pathlib import Path
 
-ROOT = Path.cwd().resolve().parent.parent.parent
-sys.path.append(str(ROOT / "src"))
+from so101_robotic_learning.bounce_rl.rewards.reward_ball_aligned_on_z_and_above_paddle import BallAlignedOnZAndAbovePaddleReward
 
-from bounce_rl.rewards.rewards import PingPongReward
-from bounce_rl.rewards.penalty_ball_below_paddle import BallBelowPaddlePenalty
-from bounce_rl.rewards.reward_ball_aligned_on_z_and_above_paddle import BallAlignedOnZAndAbovePaddleReward
+root = "../../"
+xml_path = root + "assets/mjcf/so101_new_calib copy.xml"
 
-def main():
-    model = mujoco.MjModel.from_xml_path(str(ROOT / "assets" / "mjcf" / "so101_new_calib copy.xml"))
-    data = mujoco.MjData(model)
+model = mujoco.MjModel.from_xml_path(str(xml_path))
+data = mujoco.MjData(model)
 
-    print(model.actuator_ctrlrange)
+print(model.actuator_ctrlrange)
 
-    reward_fn = BallAlignedOnZAndAbovePaddleReward(model)
+reward_fn = BallAlignedOnZAndAbovePaddleReward()
 
-    with mujoco.viewer.launch_passive(model, data) as viewer:
-        start = time.time()
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    start = time.time()
 
-        while viewer.is_running():
-            mujoco.mj_step(model, data)
-            print(reward_fn.compute(model, data))
+    while viewer.is_running():
+        mujoco.mj_step(model, data)
+        print(reward_fn.compute(model, data))
 
-            # Important en mode passif pour refléter l'état courant
-            viewer.sync()
+        # Important en mode passif pour refléter l'état courant
+        viewer.sync()
 
-            # Respect approximatif du timestep
-            time.sleep(model.opt.timestep)
-
-if __name__ == "__main__":
-    main()
+        # Respect approximatif du timestep
+        time.sleep(model.opt.timestep)
